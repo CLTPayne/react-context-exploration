2e5f55bf492ff0719221f96a46c3c7d64a19cd1e
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-01-26 15:54:09 +0000

    Add theme switch and state

--- src/App.js
+++ src/App.js
@@ -3,23 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    theme: 'theme-night'
+  }
+
+  changeTheme = () => {
+    switch (this.state.theme) {
+      case 'theme-night':
+        return this.setState({ theme: 'theme-day' });
+      case 'theme-day':
+        return this.setState({ theme: 'theme-night' });
+      default: return null;
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Night / Day
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <Toggle
+          changeTheme={this.changeTheme}
+          theme={this.state.theme}
+        />
       </div>
     );
   }
